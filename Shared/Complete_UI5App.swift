a5d088fd517c4b98798cb6c972b44abf669ab8d8
sikandarali307 <sikandar.ali307@gmail.com>
2021-10-29 09:48:19 +0500

    Initial Commit

---
+++ Shared/Complete_UI5App.swift
@@ -0,0 +1,17 @@
+//
+//  Complete_UI5App.swift
+//  Shared
+//
+//  Created by Mac on 29/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Complete_UI5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
